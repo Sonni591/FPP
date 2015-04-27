fcb88df75fdc8810ef3aac92bd7abb85d93daccd
Daniel Sonnleitner <Sonnleitner.Daniel@t-online.de>
2015-04-27 17:48:11 +0200

    GUI Entwurf
    GUI Entwurf mit JavaFX 8

---
+++ SMPLSP/src/de/oth/smplsp/view/RootLayoutController.java
@@ -0,0 +1,44 @@
+package de.oth.smplsp.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import de.oth.smplsp.Main;
+
+public class RootLayoutController {
+
+    @FXML
+    public AnchorPane inputPane;
+
+    @FXML
+    public AnchorPane pane2;
+
+    // Reference to the main application.
+    private Main main;
+
+    /**
+     * The constructor. The constructor is called before the initialize()
+     * method.
+     */
+    public RootLayoutController() {
+
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(Main main) {
+	this.main = main;
+    }
+
+}
