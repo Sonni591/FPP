0d36235e84aaf49616b1f24b746db6b47a578f0d
Daniel Sonnleitner <Sonnleitner.Daniel@t-online.de>
2015-04-28 14:00:11 +0200

    Test setzte BorderLayout

--- SMPLSP/src/de/oth/smplsp/view/RootLayoutController.java
+++ SMPLSP/src/de/oth/smplsp/view/RootLayoutController.java
@@ -1,13 +1,20 @@
 package de.oth.smplsp.view;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import de.oth.smplsp.Main;
 
 public class RootLayoutController {
 
     @FXML
-    public AnchorPane inputPane;
+    public AnchorPane anchorPane;
+
+    @FXML
+    public BorderPane borderPane;
 
     @FXML
     public AnchorPane pane2;
@@ -29,7 +36,7 @@ public class RootLayoutController {
      */
     @FXML
     private void initialize() {
-
+	showInput();
     }
 
     /**
@@ -39,6 +46,34 @@ public class RootLayoutController {
      */
     public void setMainApp(Main main) {
 	this.main = main;
+    }
+
+    public void showInput() {
+
+	FXMLLoader loader = new FXMLLoader();
+	loader.setLocation(Main.class.getResource("view/BorderInput.fxml"));
+	BorderPane inputOverview;
+	try {
+	    inputOverview = (BorderPane) loader.load();
+	    borderPane.setCenter(inputOverview);
+
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	FXMLLoader loader2 = new FXMLLoader();
+	loader2.setLocation(Main.class.getResource("view/Input.fxml"));
+	AnchorPane inputOverview2;
+	try {
+	    inputOverview2 = (AnchorPane) loader2.load();
+	    anchorPane.setBottomAnchor(anchorPane, 100.0);
+
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
     }
 
 }
